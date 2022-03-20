2db1a7b2f8e33618b78af6f03a3f4406cf2e3c4c
maltsevoi <imalceff@gmail.com>
2022-03-20 15:37:16 +0300

    ssr support

--- src/main.js
+++ src/main.js
@@ -1,4 +1,13 @@
-import { createApp } from 'vue'
+import { createSSRApp } from 'vue'
+import { createRouter } from './router'
+
 import App from './App.vue'
 
-createApp(App).mount('#app')
+//createApp(App).mount('#app')
+
+export function createApp() {
+    const app = createSSRApp(App)
+    const router = createRouter()
+    app.use(router)
+    return { app, router }
+}
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,46 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+const vuePlugin = require('@vitejs/plugin-vue')
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()]
-})
+const virtualFile = '@virtual-file'
+const virtualId = '\0' + virtualFile
+const nestedVirtualFile = '@nested-virtual-file'
+const nestedVirtualId = '\0' + nestedVirtualFile
+
+
+module.exports = {
+	plugins: [
+		vuePlugin(),
+		{
+			name: 'virtual',
+			resolveId(id) {
+				if (id === '@foo') {
+					return id
+				}
+			},
+			load(id) {
+				if (id === '@foo') {
+					return `export default { msg: 'hi' }`
+				}
+			}
+		},
+		{
+			name: 'virtual-module',
+			resolveId(id) {
+				if (id === virtualFile) {
+					return virtualId
+				} else if (id === nestedVirtualFile) {
+					return nestedVirtualId
+				}
+			},
+			load(id) {
+				if (id === virtualId) {
+					return `export { msg } from "@nested-virtual-file";`
+				} else if (id === nestedVirtualId) {
+					return `export const msg = "[success] from conventional virtual file"`
+				}
+			}
+		}
+	],
+	build: {
+		minify: false
+	}
+}
